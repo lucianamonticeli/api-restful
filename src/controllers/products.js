5d45d0e4ef3954fc08e208645debe9755739b635
Luciana Monticeli <luciana.icmp@gmail.com>
2023-09-29 10:38:29 -0300

    adicionando metodo delete

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -59,8 +59,22 @@ async function put(req, res) {
 
 }
 
+async function remove(req, res) {
+    const { id } = req.params
+  
+
+    const remove = await ProductsModel.deleteOne({ _id: id })
+    
+    const message = remove ? 'success' : 'error'
+  
+    res.send({
+      message,
+    })
+}
+
 module.exports = {
     get,
     post,
     put,
+    remove,
 }
