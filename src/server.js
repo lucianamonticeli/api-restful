1a558298c78a645db7f1e7e5d1b0cd395e31d9c2
Luciana Monticeli <luciana.icmp@gmail.com>
2023-10-02 05:00:06 -0300

    adicionando middleware de CORS

--- src/server.js
+++ src/server.js
@@ -1,6 +1,5 @@
 const express = require('express')
-//const path = require('path')
-
+const cors = require('cors')
 
 const db = require('./database/db')
 const routes = require('./routes/routes')
@@ -10,6 +9,25 @@ const app = express()
 
 //conexao com o banco de dados
 db.connect()
+
+//habilita o CORS
+const allowedOrigins = [
+    'http://127.0.0.1:5500',
+    'http://www.app.com.br',
+]
+
+app.use(cors({
+    origin: function (origin, callback) {
+        let allowed = true
+
+        // permite requests sem 'origin' - exemplo: mobile apps
+        if (!origin)allowed = true
+        
+        if (!allowedOrigins.includes(origin)) allowed = false
+
+        callback(null, allowed)
+    }
+}))
 
 
 //habilita server para receber dados json
